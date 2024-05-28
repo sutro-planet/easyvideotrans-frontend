41f7ad3c30bd69d10b356b350689648d5466625d
kitety <aizaizuori@foxmail.com>
2024-05-28 21:36:54 +0800

    feat: init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+};
