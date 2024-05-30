dab4c24f1ffacb26dc660e8a8f88a8d801dea1b1
kitety <aizaizuori@foxmail.com>
2024-05-30 16:57:40 +0800

    feat: TDK完善

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,18 +1,3 @@
-export const metadata = {
-  title: 'Pytvzhen',
-  description: 'Pytvzhen 最快的英文视频转中文方案',
-  keywords: [
-    'Pytvzhen 最快的英文视频转中文方案',
-    'Pytvzhen',
-    '视频转换',
-    '视频翻译',
-    '视频转中文',
-    '视频转英文',
-    '视频转日语',
-    '视频转外语',
-  ],
-};
-
 import Hero from '@/components/hero-home';
 import Features from '@/components/features-home';
 import Cta from '@/components/cta';
